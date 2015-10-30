60758f414a34121d5bc6de28b3a5a21930d2cc28
LiquidSmokeX64 <liquidsmokex64@gmail.com>
2015-10-29 17:03:44 -0700

    Odex App

--- Android.mk
+++ Android.mk
@@ -16,4 +16,7 @@ LOCAL_PACKAGE_NAME := InCallUI
 LOCAL_CERTIFICATE := shared
 LOCAL_PRIVELEGED_MODULE := false
 
+# Enable dexpreopt to speed boot time
+WITH_DEXPREOPT := true
+
 include $(BUILD_PACKAGE)
